e7d760ee610dfd675f2f6ffb267c6977d864540d
Ubuntu <ubuntu@ip-172-31-51-180.ec2.internal>
2023-07-13 17:49:29 +0000

    test vpc created

--- main.tf
+++ main.tf
@@ -9,5 +9,14 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region = var.aws_region
+}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "test_vpc"
+  }
 }
